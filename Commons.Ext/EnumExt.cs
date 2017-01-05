9a868425e2692aed2c571999436ed97291072043
Martin Holly <martin.holly.33@gmail.com>
2017-01-05 17:25:59 -0500

    Added generic extension to get all Enum values
    The generic extension method GetValues<T>() is analogous to Enum.GetValues(Type t)
    except that this method yieds a strongly typed IEnumerable<T> result.

---
+++ Commons.Ext/EnumExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Commons.Ext
+{
+    public static class EnumExt
+    {
+        #region PUBLIC METHODS
+        /// <summary>
+        /// Returns an <see cref="IEnumerable{T}"/> of all possible values of enum <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T">Must be an enum type</typeparam>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
+        /// <exception cref="ArgumentException">Thrown if <typeparamref name="T"/> is not an enum type</exception>
+        public static IEnumerable<T> GetValues<T>() 
+            where T : struct
+        {
+            var type = typeof(T);
+            if (!type.IsEnum)
+                throw new ArgumentException("Generic argument bust be an enum type", "T");
+
+            return Enum
+                .GetValues(type)
+                .Cast<T>();
+        }
+        #endregion
+    }
+}
